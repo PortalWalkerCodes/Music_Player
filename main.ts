656fbf3813ea79ca3d09dc56b7cf2e8183b3c782
Atticuslaysviolin <150387049+Atticuslaysviolin@users.noreply.github.com>
2023-12-18 13:54:04 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("E B - C5 B - A F ", 120), music.PlaybackMode.UntilDone)
 })
+input.onButtonPressed(Button.AB, function () {
+    music.play(music.stringPlayable("E5 D C D E - E - E - E D - D - D - E - G - G E D C D E - E - E - E D D E D C C5 C", 200), music.PlaybackMode.UntilDone)
+    basic.showString("Marry had a little lamb")
+})
 input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("E D C D E - E - ", 120), music.PlaybackMode.UntilDone)
 })
-input.onButtonPressed(Button.AB, function () {
-    music.play(music.stringPlayable("E5 D C D E - E - E - E D - D - D - E - G - G E D C D E - E - E - E D D E D C C5 C", 200), music.PlaybackMode.UntilDone)
-})
+basic.showString("Push a button")
 basic.forever(function () {
 	
 })
